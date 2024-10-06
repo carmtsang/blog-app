bcbc191853b50f9394d1ba39d077e76226799120
carmtsang <carmtsang89@hotmail.com>
2024-10-05 17:36:16 -0700

    install uuid

--- src/app/lib/placeholder-data.js
+++ src/app/lib/placeholder-data.js
@@ -1,46 +1,86 @@
 const posts = [
   {
-    id: "NKCRKDNUII",
-    title: "Blog Post 1",
-    content: "This is content from blog post 1",
-    date: "",
-    user: "Carmen T",
+    id: "a5e08164-b12b-4e72-9191-2f7daadb4e88",
+    title: "Post Title 1",
+    content:
+      "Attack the child pee on walls it smells like breakfast so purrr purr littel cat, little cat purr purr so lick yarn hanging out of own butt scratch so owner bleeds.",
+    date: "276761",
+    user: "Amos T",
   },
   {
-    id: "90JY7H4G69",
-    title: "Blog Post 2",
-    content: "This is content from blog post 2",
-    date: "",
-    user: "Carmen T",
+    id: "6bd38af3-8a95-4a7a-add7-fd69c6b8f124",
+    title: "Post Title 2",
+    content:
+      "Eats owners hair then claws head hide from vacuum cleaner meow meow you are my owner so here is a dead bird yet shred all toilet paper and spread around the house headbutt owner's knee.",
+    date: "911166",
+    user: "Amos T",
   },
   {
-    id: "RJHDPN3IZ3",
-    title: "Blog Post 3",
-    content: "This is content from blog post 3",
-    date: "",
-    user: "Carmen T",
+    id: "836b0611-e82e-4b5e-8ab9-8e9f98e43549",
+    title: "Post Title 3",
+    content:
+      "Walk on car leaving trail of paw prints on hood and windshield. If it smells like fish eat as much as you wish sleeping in the box and chase ball of string so murr.",
+    date: "393288",
+    user: "Astro T",
   },
   {
-    id: "19BL018EBV",
-    title: "Blog Post 4",
-    content: "This is content from blog post 4",
-    date: "",
-    user: "Carmen T",
+    id: "b08212a4-fd01-4d39-892c-15d49b8a96a4",
+    title: "Post Title 4",
+    content:
+      "Your pillow is now my pet bed cat gets stuck in tree firefighters try to get cat down firefighters get stuck in tree cat eats firefighters' slippers flee in terror at cucumber.",
+    date: "1047019",
+    user: "Frank W",
   },
   {
-    id: "6JXA6ZLRSQ",
-    title: "Blog Post 5",
-    content: "This is content from blog post 5",
-    date: "",
-    user: "Carmen T",
+    id: "bd0f47c5-3cd0-48ee-9b52-104a914e2354",
+    title: "Post Title 5",
+    content:
+      "Instantly break out into full speed gallop across the house for no reason thug cat murf pratt ungow ungow and throwup on your pillow, or stare at wall turn and meow stare at wall.",
+    date: "544869",
+    user: "Chip W",
   },
   {
-    id: " UJFK5EDORT",
-    title: "",
-    content: "This is content from blog post 6",
-    date: "",
-    user: "Carmen T",
+    id: "e7c26aa0-76f7-4037-b341-4a794c9c8f64",
+    title: "Post Title 6",
+    content:
+      "Mewl for food at 4am hiiiiiiiiii feed me now. Find something else more interesting hiding behind the couch until lured out by a feathery toy and licks your face so pelt around the house and up and down stairs chasing phantoms scratch me now! stop scratching me!",
+    date: "642458",
+    user: "Amos T",
+  },
+  {
+    id: "2375388b-3b07-401b-9f70-fd69b4ef5ead",
+    title: "Post Title 7",
+    content:
+      "Meow loudly just to annoy owners stare at ceiling light favor packaging over toy for meow and walk away.",
+    date: "304066",
+    user: "Tobi Y",
+  },
+  {
+    id: "b1a5ffb6-1152-4a9f-a997-9f4e66e140af",
+    title: "Post Title 8",
+    content:
+      "Hiding behind the couch until lured out by a feathery toy snuggles up to shoulders or knees and purrs you to sleep destroy dog need to chase tail.",
+    date: "1005474",
+    user: "Astro T",
+  },
+  {
+    id: "7f9622bb-eb4d-4f42-b66a-7dfa28f5e32a",
+    title: "Post Title 9",
+    content:
+      "Try to jump onto window and fall while scratching at wall chase ball of string, for stare out cat door then go back inside so eat the rubberband yet pelt around the house and up and down stairs chasing phantoms lies down i'm bored inside, let me out i'm lonely outside.",
+    date: "113002",
+    user: "Astro T",
+  },
+  {
+    id: "724688be-3911-4bf7-8717-4ac8931daf8c",
+    title: "Post Title 10",
+    content:
+      "Hack the fat cat sat on the mat bat away with paws so paw at beetle and eat it before it gets away so carrying out surveillance on the neighbour's dog and drink water out of the faucet, annoy owner until he gives you food say meow repeatedly until belly rubs, feels good so play riveting piece on synthesizer keyboard. Poop on the floor, break a planter, sprint, eat own hair, vomit hair, hiss, chirp at birds, eat a squirrel, hide from fireworks, lick toe beans, attack christmas tree ask for petting.",
+    date: "632982",
+    user: "Amos T",
   },
 ];
 
-export default posts;
+module.exports = {
+  posts,
+};
